2395c07f1d705f87d7d24248382ac5bf639f8fa5
Zeyu <haozai_03@hotmail.com>
2019-04-16 16:39:30 +0000

    finished exercis 1-3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,14 @@
 require_relative '../setup'
 require_relative './exercise_1'
+require 'pry'
 
 puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(id = 1)
+puts @store1.name
+@store2 = Store.find(id=2)
+@store1.name = "Nike"
+@store1.save  
+
